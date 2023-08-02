0a6ce8850d5c924bfede718ccff4badfca70bea5
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-08-03 00:22:51 +0530

    first commit

--- models/post.js
+++ models/post.js
@@ -9,11 +9,17 @@ user:{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
 },
-
+//include the array of ids of all comments in this Post schema itself
+comments:[
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }
+]
 },{
     timestamps:true
 });
 
-const Post = mongoose.Model('Post',postSchema);
+const Post = mongoose.model('Post',postSchema);
 
 module.exports = Post;  
