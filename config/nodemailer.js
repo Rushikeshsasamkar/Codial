f0424aabc318d80ae741112c31757d57a9fccc51
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-08-14 19:59:28 +0530

    SMTP mailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -9,7 +9,7 @@ let transporter = nodemailer.createTransport({
     secure: false,
     auth:{
         user:'technicalprogramming111@gmail.com',
-        pass:'Rushi@9422'
+        pass:'******'
     }
 });
 
